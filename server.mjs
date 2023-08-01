ff4469bf4bc86393813c318df5b8201f85a9918e
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-08-01 18:48:11 +0500

    adding vercel file

--- server.mjs
+++ server.mjs
@@ -7,7 +7,17 @@ config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = ['https://smitproducts.netlify.app', 'https://example.com'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 app.use(express.json());
 
 // URI
